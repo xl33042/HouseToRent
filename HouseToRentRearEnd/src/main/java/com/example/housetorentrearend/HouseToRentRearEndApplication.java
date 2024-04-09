dfd854c6b89d5b1f31808c7729088893789d6d71
xiao33042 <xiao33042.@gmail.com>
2024-04-09 11:27:48 +0800

    后端

---
+++ HouseToRentRearEnd/src/main/java/com/example/housetorentrearend/HouseToRentRearEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.housetorentrearend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseToRentRearEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseToRentRearEndApplication.class, args);
+    }
+
+}
