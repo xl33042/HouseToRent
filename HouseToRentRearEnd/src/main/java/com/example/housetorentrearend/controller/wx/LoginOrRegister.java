6b7c491762db631c40030d103334dc56db23162d
AChinaProgrammer <A022984053@outlook.com>
2024-04-10 09:38:12 +0800

    xml引入了腾讯短信验证，控制器增加了微信登录一方

---
+++ HouseToRentRearEnd/src/main/java/com/example/housetorentrearend/controller/wx/LoginOrRegister.java
@@ -0,0 +1,14 @@
+package com.example.housetorentrearend.controller.wx;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/wx/LoginOrRegister")
+public class LoginOrRegister {
+
+    
+
+}
---
+++ HouseToRentRearEnd/src/main/java/com/example/housetorentrearend/entity/wx/PhoneSms.java
@@ -0,0 +1,2 @@
+package com.example.housetorentrearend.entity.wx;public class PhoneSms {
+}
