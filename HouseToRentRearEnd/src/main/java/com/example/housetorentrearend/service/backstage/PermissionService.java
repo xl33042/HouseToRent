ac185d3e0adb4bf4e779b0a6718a12d74abafe49
AChinaProgrammer <133077804+AChinaProgrammer@users.noreply.github.com>
2024-04-13 10:48:30 +0800

    Delete HouseToRentRearEnd directory

--- HouseToRentRearEnd/src/main/java/com/example/housetorentrearend/service/backstage/PermissionService.java
+++
@@ -1,7 +0,0 @@
-package com.example.housetorentrearend.service.backstage;
-
-import java.util.List;
-
-public interface PermissionService {
-    public List<String> findByRoleId(List<Integer> roleIds);
-}
--- HouseToRentRearEnd/src/main/java/com/example/housetorentrearend/service/wx/PersonInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.example.housetorentrearend.service.wx;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.housetorentrearend.entity.PersonInfoVO;
-
-public interface PersonInfoService extends IService<PersonInfoVO> {
-    PersonInfoVO verifyLoginOrRegistration(String mobile, String password);
-}
